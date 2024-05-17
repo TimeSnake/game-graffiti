9a71bf807813b0d029ab9c5fd6f1d6d01b6f66f3
funzter <cavehive@protonmail.com>
2024-05-17 21:41:54 +0200

    minor

--- src/main/java/de/timesnake/game/graffiti/server/Plugin.java
+++ src/main/java/de/timesnake/game/graffiti/server/Plugin.java
@@ -4,16 +4,11 @@
 
 package de.timesnake.game.graffiti.server;
 
-import de.timesnake.library.basic.util.LogHelper;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.game.util.user.Plugin {
 
-  public static final Plugin GRAFFITI = new Plugin("Graffiti", "GGF",
-      LogHelper.getLogger("Graffiti", Level.INFO));
+  public static final Plugin GRAFFITI = new Plugin("Graffiti", "GGF");
 
-  protected Plugin(String name, String code, Logger logger) {
-    super(name, code, logger);
+  protected Plugin(String name, String code) {
+    super(name, code);
   }
 }
